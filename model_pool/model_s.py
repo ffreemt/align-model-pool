16acc72b83f0e938673bf800bc9f943df04f1cab
freemt <yucongo+fmt@gmail.com>
2022-01-30 13:14:27 +0800

    Update alive_bar_on

--- model_pool/model_s.py
+++ model_pool/model_s.py
@@ -12,17 +12,21 @@ from alive_progress import alive_bar
 from logzero import logger
 
 
-def load_model_s():
+def load_model_s(alive_bar_on=True):
     """Load local model_s if present, else fetch from hf.co."""
     file_loc = "radiobee/model_s"
     if Path(file_loc).exists():
         # raise Exception(f"File {file_loc} does not exist.")
 
-        with alive_bar(1, title=" Loading model_s, takes ~30 secs ...", length=3) as progress_bar:
-            model = joblib.load(file_loc)
+        if alive_bar_on:
+            with alive_bar(1, title=" Loading model_s, takes ~30 secs ...", length=3) as progress_bar:
+                model = joblib.load(file_loc)
 
-            # model_s = pickle.load(open(file_loc, "rb"))
-            progress_bar()  # pylint: disable=not-callable
+                # model_s = pickle.load(open(file_loc, "rb"))
+                progress_bar()  # pylint: disable=not-callable
+        else:
+            logger.info("Loading local model, it may take a while")
+            model = joblib.load(file_loc)
 
         return model
 
@@ -30,10 +34,13 @@ def load_model_s():
         "Fetching and caching model_s from huggingface.co... "
         "The first time may take a while depending on your net."
     )
-    with alive_bar(1, title=" Subsequent loading takes ~20 secs ...", length=3) as progress_bar:
+    if alive_bar_on:
+        with alive_bar(1, title=" Subsequent loading takes ~20 secs ...", length=3) as progress_bar:
+            model = joblib.load(cached_download(hf_hub_url("mikeee/model_s", "model_s")))
+            progress_bar()  # pylint: disable=not-callable
+    else:
+        logger.info("Loading model from hf-hub, it may take a while")
         model = joblib.load(cached_download(hf_hub_url("mikeee/model_s", "model_s")))
-        progress_bar()  # pylint: disable=not-callable
-
     return model
 
 

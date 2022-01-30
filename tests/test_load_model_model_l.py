7b3a3de86b7183c9e1e6af001fe2e1603f9baecb
freemt <yucongo+fmt@gmail.com>
2022-01-30 15:25:03 +0800

    Update test_model_l

--- tests/test_load_model_model_l.py
+++ tests/test_load_model_model_l.py
@@ -2,14 +2,15 @@
 from model_pool.load_model import load_model
 from logzero import logger
 
-try:
-    clas = load_model("model-l", alive_bar_on=False)
-except Exception as exc:
-    logger.error("load_model('model-l'): %s", exc)
-    raise SystemExit(1) from exc
-
 
 def test_model_l1():
     """Test model-l 1."""
+    try:
+        clas = load_model("model-l", alive_bar_on=False)
+    except Exception as exc:
+        logger.error("load_model('model-l'): %s", exc)
+        # raise SystemExit(1) from exc
+        assert False, str(exc)
+
     res = clas("test", ["test", "测试"], multi_lable=True)
     assert res.keys() == ["labels", "scores", "sequence"]

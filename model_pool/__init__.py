d5ac4d08837498a0fefa0284d1dce8a1efdb6ee9
freemt <yucongo+fmt@gmail.com>
2022-02-17 17:34:10 +0800

    Bump to 0.1.1

--- model_pool/__init__.py
+++ model_pool/__init__.py
@@ -1,5 +1,5 @@
 """Init."""
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 from .model_pool import model_pool
 from .fetch_check_aux import fetch_check_aux
 
--- tests/test_model_pool.py
+++ tests/test_model_pool.py
@@ -6,7 +6,7 @@ from model_pool import model_pool
 
 def test_version():
     """Test version."""
-    assert __version__ == "0.1.0"
+    assert __version__ == "0.1.1"
 
 
 def test_sanity():

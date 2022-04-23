7d795a926f762f439b5e1bded80e9a16d717aa13
freemt <yucongo+fmt@gmail.com>
2022-04-23 18:43:45 +0800

    Update test_fetch_check_aux for cases when /root not writable

--- tests/test_fetch_check_aux.py
+++ tests/test_fetch_check_aux.py
@@ -1,7 +1,18 @@
 """Test fetch_check_aux."""
 from model_pool import fetch_check_aux
+from logzero import logger
 
 
 def test_fetch_check_aux1():
     """Test fetch_check_aux 1"""
-    assert fetch_check_aux("/root")
+    try:
+        res = fetch_check_aux("/root")
+    except PermissionError:
+        # may not be able to write to /root
+        # write to ~
+        res = fetch_check_aux()
+    except Exception:
+        logger.exception("")
+        raise
+
+    assert res
